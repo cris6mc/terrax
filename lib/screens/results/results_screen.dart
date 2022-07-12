0bb28104821de92f78114bdfd87151026a857cd6
Kevin <kevinjhosepct@gmail.com>
2022-07-11 20:24:22 -0500

    Completed results screen

---
+++ lib/screens/results/results_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({Key? key}) : super(key: key);
+  static String routeName = "/test";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Terrax", style: TextStyle(color: Colors.white),),
+        backgroundColor: Color.fromARGB(255, 7, 92, 59),
+        // backgroundColor: Color.fromARGB(255, 2, 4, 37),
+      ),  
+      body: const Body(),
+    );
+  }
+}
